bc848fc92fa2b15fb481fc06f82f236107d72ca9
Apurba <apurba@excellencetech.com>
2020-12-17 13:31:14 +0530

    Update

--- projects/minieditor/src/lib/minieditor.component.html.ts
+++ projects/minieditor/src/lib/minieditor.component.html.ts
@@ -4,48 +4,48 @@ export default
     <div class="controls-left">
         <span class="left">
             <button class="button" [class.active]="bold" (click)="insertBold()">
-                <i class="vc-icons">&#xe905;</i>
+                <i class="vc-icons">&#xe906;</i>
             </button>
             <button class="button" [class.active]="italic" (click)="insertItalic()">
-                <i class="vc-icons">&#xe90c;</i>
+                <i class="vc-icons">&#xe90d;</i>
             </button>
             <button class="button" [class.active]="strikeThrough" (click)="insertStrikeThrough()">
-                <i class="vc-icons">&#xe910;</i>
+                <i class="vc-icons">&#xe911;</i>
             </button>
             <button class="button " [class.active]="underline" (click)="insertUnderLine()">
-                <i class="vc-icons">&#xe914;</i>
+                <i class="vc-icons">&#xe915;</i>
             </button>
             <button *ngIf="link" class="button"><i class="vc-icons" (click)="openDialog()">&#xe90d;</i></button>
             <button class="button" (click)="insertSupTag()" [class.active]="supTag">
-                <i class="vc-icons">&#xe913;</i>
+                <i class="vc-icons">&#xe914;</i>
             </button>
             <button class="button" (click)="insertSubTag()" [class.active]="subTag">
-                <i class="vc-icons">&#xe912;</i>
+                <i class="vc-icons">&#xe913;</i>
             </button>
             <button class="button" (click)="insertBlockQuote()" [class.active]="blockquote">
-                <i class="vc-icons">&#xe908;</i>
+                <i class="vc-icons">&#xe909;</i>
             </button>
             <span *ngIf="list" style="display: flex; justify-content: space-around; flex-direction: row">
                 <div class="borderRgt"></div>
                 <button class="button" [class.active]="unorderedList" (click)="insertUnorderedList()"
                     [style.display]="'inline'">
-                    <i class="vc-icons">&#xe90e;</i>
+                    <i class="vc-icons">&#xe90f;</i>
                 </button>
                 <button class="button" [class.active]="orderedList" (click)="insertOrderedList()"
                     [style.display]="'inline'">
-                    <i class="vc-icons">&#xe90f;</i>
+                    <i class="vc-icons">&#xe910;</i>
                 </button>
             </span>
             <span *ngIf="indent" style="display: flex; justify-content: space-around; flex-direction: row">
                 <div class="borderRgt"></div>
                 <button class="button" [class.active]="increaseIndent" (click)="increaseIndentation()">
-                    <i class="vc-icons">&#xe90b;</i>
+                    <i class="vc-icons">&#xe90c;</i>
                 </button>
                 <button class="button" [class.active]="decreaseIndent" (click)="decreaseIndentation()">
-                    <i class="vc-icons">&#xe90a;</i>
+                    <i class="vc-icons">&#xe90b;</i>
                 </button>
                 <button class="button" [class.active]="colorPicker" (click)="applyColor($event)">
-                    <i class="vc-icons">&#xe907;</i>
+                    <i class="vc-icons">&#xe908;</i>
                 </button>
                 <div class="color-picker" *ngIf="colorPicker" [style.left.px]="popoverLeft" [style.top.px]="popoverTop">
                     <ul class="colors">
@@ -66,17 +66,17 @@ export default
             <span *ngIf="mentionedNames?.length > 0">
                 <div class="borderRgt"></div>
                 <button class="button bluetxt" (click)="insChar('@')" [style.display]="'inline'"><i
-                        class="vc-icons">&#xe904;</i></button>
+                        class="vc-icons">&#xe905;</i></button>
             </span>
             <span *ngIf="mentionedDates?.length > 0">
                 <div class="borderRgt"></div>
                 <button class="button bluetxt" (click)="insChar('#')" [style.display]="'inline'"><i
-                        class="vc-icons">&#xe909;</i></button>
+                        class="vc-icons">&#xe90a;</i></button>
             </span>
             <span *ngIf="file">
-                <button class="button greentxt attachedBtn">
-                    <input type="file" (change)="onFileChange($event)"><span><i class="vc-icons">&#xe922;</i></span>
-                </button>
+                <span class="button greentxt attachedBtn">
+                    <input type="file" (change)="onFileChange($event)"><span><i class="vc-icons">&#xe900;</i></span>
+                </span>
             </span>
         </div>
     </div>
